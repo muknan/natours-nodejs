f1788fad9699687a8cdffc738dd9cc243d8433aa
muknan <muk.msi@gmail.com>
2024-07-02 13:29:19 +0530

    Setting up eslint

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const app = require('./app');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-console.log(process.env);
+// console.log(process.env);
 
 // 4) START SERVER
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, '127.0.0.1', () => {
+app.listen(port, () => {
   console.log(`Starting server on port ${port}... `);
 });
